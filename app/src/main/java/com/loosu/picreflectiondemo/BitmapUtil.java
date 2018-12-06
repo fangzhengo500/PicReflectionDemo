5a7d549edafe6b639260b43490b27766cd0d4787
LuWei <luwei@ykecf.cn>
2018-12-06 15:11:39 +0800

    阴影透明度调试

--- app/src/main/java/com/loosu/picreflectiondemo/BitmapUtil.java
+++ app/src/main/java/com/loosu/picreflectiondemo/BitmapUtil.java
@@ -2,17 +2,19 @@ package com.loosu.picreflectiondemo;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Shader;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 public class BitmapUtil {
     public static Bitmap createBitmapShadow(Bitmap sourceBmp, int shadowHeight) {
-        return createBitmapShadow(sourceBmp, shadowHeight, 0x70ffffff, 0x0000000);
+        return createBitmapShadow(sourceBmp, shadowHeight, 255, 0);
     }
 
     /**
@@ -20,18 +22,18 @@ public class BitmapUtil {
      *
      * @param sourceBmp    原图
      * @param shadowHeight 阴影高度
-     * @param startColor   startColor
-     * @param endColor     endColor
+     * @param startAlpha   Alpha component \([0..255]\) of the color
+     * @param endAlpha     Alpha component \([0..255]\) of the color
      * @return 阴影图片
      */
     @Nullable
-    public static Bitmap createBitmapShadow(Bitmap sourceBmp, int shadowHeight, int startColor, int endColor) {
+    public static Bitmap createBitmapShadow(@NonNull Bitmap sourceBmp, int shadowHeight, int startAlpha, int endAlpha) {
         int x = 0;
         int y = Math.max(0, Math.min(sourceBmp.getHeight() - shadowHeight, sourceBmp.getHeight())); // 0 <= y <= bmp.getHeight;
         int width = sourceBmp.getWidth();
         int height = Math.min(shadowHeight, sourceBmp.getHeight() - y);
 
-        if (height <= 0){
+        if (height <= 0) {
             return null;
         }
 
@@ -40,7 +42,8 @@ public class BitmapUtil {
         Bitmap shadowBmp = Bitmap.createBitmap(sourceBmp, x, y, width, height, matrix, false);
 
         LinearGradient shader = new LinearGradient(0, 0, 0, shadowBmp.getHeight(),
-                startColor, endColor,
+                Color.argb(startAlpha, 0, 0, 0),
+                Color.argb(endAlpha, 0, 0, 0),
                 Shader.TileMode.MIRROR);
         Paint paint = new Paint();
         paint.setShader(shader);
--- app/src/main/java/com/loosu/picreflectiondemo/MainActivity.java
+++ app/src/main/java/com/loosu/picreflectiondemo/MainActivity.java
@@ -4,8 +4,6 @@ import android.graphics.Bitmap;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -15,12 +13,19 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
     private static final String TAG = "MainActivity";
 
-    //
+    private DrawerLayout mLayoutDrawer;
+
+    // main content
     private ImageView mIvSource;
     private ImageView mIvShadow;
+
+    // main menu
     private SeekBar mSeekShadowHeight;
     private TextView mTvShadowHeight;
-    private DrawerLayout mLayoutDrawer;
+    private SeekBar mSeekStartAlpha;
+    private TextView mTvStartAlpha;
+    private SeekBar mSeekEndAlpha;
+    private TextView mTvEndAlpha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,60 +47,65 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         // main menu
         mSeekShadowHeight = findViewById(R.id.seek_shadow_height);
         mTvShadowHeight = findViewById(R.id.tv_shadow_height);
+
+        mSeekStartAlpha = findViewById(R.id.seek_start_alpha);
+        mTvStartAlpha = findViewById(R.id.tv_start_alpha);
+
+        mSeekEndAlpha = findViewById(R.id.seek_end_alpha);
+        mTvEndAlpha = findViewById(R.id.tv_end_alpha);
     }
 
     private void initView(Bundle savedInstanceState) {
         mIvSource.post(new Runnable() {
             @Override
             public void run() {
-                int shadowHeight = mIvSource.getHeight() / 4;
-                updateImageShadow(shadowHeight);
+                int shadowHeight = mIvSource.getHeight();
+                int startAlpha = 255;
+                int endAlpha = 0;
+                updateImageShadow(shadowHeight, startAlpha, endAlpha);
 
                 mSeekShadowHeight.setProgress(shadowHeight);
                 mTvShadowHeight.setText(String.valueOf(shadowHeight));
+
+                mSeekStartAlpha.setProgress(startAlpha);
+                mTvStartAlpha.setText(String.valueOf(startAlpha));
+
+                mSeekEndAlpha.setProgress(endAlpha);
+                mTvEndAlpha.setText(String.valueOf(endAlpha));
             }
         });
     }
 
-    private void updateImageShadow(int shadowHeight) {
+    private void updateImageShadow(int shadowHeight, int startAlpha, int endAlpha) {
         Bitmap source = ViewUtil.getBitmapByView(mIvSource);
-        Bitmap shadow = BitmapUtil.createBitmapShadow(source, shadowHeight);
+        Bitmap shadow = BitmapUtil.createBitmapShadow(source, shadowHeight, startAlpha, endAlpha);
         mIvShadow.setImageBitmap(shadow);
     }
 
     private void initListener(Bundle savedInstanceState) {
-        mSeekShadowHeight.setOnSeekBarChangeListener(this);
-        mLayoutDrawer.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                mLayoutDrawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
-            }
+        mLayoutDrawer.addDrawerListener(mDrawerListener);
+        mLayoutDrawer.setOnTouchListener(mDrawerTouchListener);
 
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                mLayoutDrawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
-            }
-        });
-        // 点击除开侧边栏的区域会收起侧边栏。
-        mLayoutDrawer.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        mLayoutDrawer.closeDrawers();
-                        break;
-                }
-                return false;
-            }
-        });
+        mSeekShadowHeight.setOnSeekBarChangeListener(this);
+        mSeekStartAlpha.setOnSeekBarChangeListener(this);
+        mSeekEndAlpha.setOnSeekBarChangeListener(this);
+
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         switch (seekBar.getId()) {
             case R.id.seek_shadow_height:
-                updateImageShadow(progress);
+                updateImageShadow(mSeekShadowHeight.getProgress(), mSeekStartAlpha.getProgress(), mSeekEndAlpha.getProgress());
                 mTvShadowHeight.setText(String.valueOf(progress));
+                break;
+            case R.id.seek_start_alpha:
+                updateImageShadow(mSeekShadowHeight.getProgress(), mSeekStartAlpha.getProgress(), mSeekEndAlpha.getProgress());
+                mTvStartAlpha.setText(String.valueOf(progress));
+                break;
+            case R.id.seek_end_alpha:
+                updateImageShadow(mSeekShadowHeight.getProgress(), mSeekStartAlpha.getProgress(), mSeekEndAlpha.getProgress());
+                mTvEndAlpha.setText(String.valueOf(progress));
                 break;
         }
     }
@@ -109,4 +119,30 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     public void onStopTrackingTouch(SeekBar seekBar) {
 
     }
+
+    // 处理滑动冲突
+    private DrawerLayout.SimpleDrawerListener mDrawerListener = new DrawerLayout.SimpleDrawerListener() {
+        @Override
+        public void onDrawerOpened(View drawerView) {
+            mLayoutDrawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
+        }
+
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            mLayoutDrawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+        }
+    };
+
+    // 点击除开侧边栏的区域会收起侧边栏。
+    private View.OnTouchListener mDrawerTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    mLayoutDrawer.closeDrawers();
+                    break;
+            }
+            return false;
+        }
+    };
 }
