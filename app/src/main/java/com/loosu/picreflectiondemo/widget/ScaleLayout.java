c760d2d134b32cdb00294580e056df8abbcb0e6a
LuWei <luwei@ykecf.cn>
2018-12-07 17:33:24 +0800

    高级货色调试

--- app/src/main/java/com/loosu/picreflectiondemo/widget/ScaleLayout.java
+++ app/src/main/java/com/loosu/picreflectiondemo/widget/ScaleLayout.java
@@ -1,12 +1,20 @@
 package com.loosu.picreflectiondemo.widget;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
 
+import java.util.ArrayList;
+
 public class ScaleLayout extends FrameLayout {
+    private static final String TAG = "ScaleLayout";
+
+    private final ArrayList<View> mMatchParentChildren = new ArrayList<>(1);
 
     private int mWidthScale = 4;
     private int mHeightScale = 3;
@@ -34,25 +42,106 @@ public class ScaleLayout extends FrameLayout {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int count = getChildCount();
 
-//        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-//        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-//
-//        int width = MeasureSpec.getSize(widthMeasureSpec);
-//        int height = MeasureSpec.getSize(heightMeasureSpec);
-//        if (0 == mWidthScale || 0 == mHeightScale) {
-//            setMeasuredDimension(width, height);
-//        } else {
-//            if (width < height * mWidthScale / mHeightScale) {
-//                int finalHeight = width * mHeightScale / mWidthScale;
-//                setMeasuredDimension(width, finalHeight);
-//                measureChildren(MeasureSpec.makeMeasureSpec(width, widthMode), MeasureSpec.makeMeasureSpec(finalHeight, heightMode));
-//            } else {
-//                int finalWidth = height * mWidthScale / mHeightScale;
-//                setMeasuredDimension(finalWidth, height);
-//                measureChildren(MeasureSpec.makeMeasureSpec(finalWidth, widthMode), MeasureSpec.makeMeasureSpec(height, heightMode));
-//            }
-//        }
+        Log.d(TAG, MeasureSpec.toString(widthMeasureSpec) + ", " + MeasureSpec.toString(heightMeasureSpec));
+        int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+
+        // 如果不是 MeasureSpec.EXACTLY 就去测量child
+        final boolean measureMatchParentChildren = modeWidth != MeasureSpec.EXACTLY || modeHeight != MeasureSpec.EXACTLY;
+        mMatchParentChildren.clear();
+
+        int maxHeight = 0;
+        int maxWidth = 0;
+        int childState = 0;
+
+        for (int i = 0; i < count; i++) {
+            final View child = getChildAt(i);
+            if (getMeasureAllChildren() || child.getVisibility() != GONE) {
+                measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0);
+                final LayoutParams lp = (LayoutParams) child.getLayoutParams();
+                maxWidth = Math.max(maxWidth,
+                        child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin);
+                maxHeight = Math.max(maxHeight,
+                        child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin);
+                childState = combineMeasuredStates(childState, child.getMeasuredState());
+                if (measureMatchParentChildren) {
+                    if (lp.width == LayoutParams.MATCH_PARENT ||
+                            lp.height == LayoutParams.MATCH_PARENT) {
+                        mMatchParentChildren.add(child);
+                    }
+                }
+            }
+        }
+
+        // Check against our minimum height and width
+        maxHeight = Math.max(maxHeight, getSuggestedMinimumHeight());
+        maxWidth = Math.max(maxWidth, getSuggestedMinimumWidth());
+
+        // Check against our foreground's minimum height and width
+        final Drawable drawable = getForeground();
+        if (drawable != null) {
+            maxHeight = Math.max(maxHeight, drawable.getMinimumHeight());
+            maxWidth = Math.max(maxWidth, drawable.getMinimumWidth());
+        }
+
+
+        // by luwei start
+        // setMeasuredDimension(
+        // resolveSizeAndState(maxWidth, widthMeasureSpec, childState),
+        // resolveSizeAndState(maxHeight, heightMeasureSpec, childState << MEASURED_HEIGHT_STATE_SHIFT));
+
+        if (0 == mWidthScale || 0 == mHeightScale) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        } else {
+            if (maxWidth < maxHeight * mWidthScale / mHeightScale) {
+                int finalHeight = maxWidth * mHeightScale / mWidthScale;
+                setMeasuredDimension(maxWidth, finalHeight);
+                measureChildren(MeasureSpec.makeMeasureSpec(maxWidth, MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(finalHeight, MeasureSpec.EXACTLY));
+            } else {
+                int finalWidth = maxHeight * mWidthScale / mHeightScale;
+                setMeasuredDimension(finalWidth, maxHeight);
+                measureChildren(MeasureSpec.makeMeasureSpec(finalWidth, MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(maxHeight, MeasureSpec.EXACTLY));
+            }
+        }
+        // by luwei end
+
+        count = mMatchParentChildren.size();
+        if (count > 1) {
+            for (int i = 0; i < count; i++) {
+                final View child = mMatchParentChildren.get(i);
+                final MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+
+                final int childWidthMeasureSpec;
+                if (lp.width == LayoutParams.MATCH_PARENT) {
+                    final int width = Math.max(0, getMeasuredWidth()
+                            - lp.leftMargin - lp.rightMargin);
+                    childWidthMeasureSpec = MeasureSpec.makeMeasureSpec(
+                            width, MeasureSpec.EXACTLY);
+                } else {
+                    childWidthMeasureSpec = getChildMeasureSpec(getMeasuredWidth(),
+                            lp.leftMargin + lp.rightMargin,
+                            lp.width);
+                }
+
+                final int childHeightMeasureSpec;
+                if (lp.height == LayoutParams.MATCH_PARENT) {
+                    final int height = Math.max(0, getMeasuredHeight()
+                            - lp.topMargin - lp.bottomMargin);
+                    childHeightMeasureSpec = MeasureSpec.makeMeasureSpec(
+                            height, MeasureSpec.EXACTLY);
+                } else {
+                    childHeightMeasureSpec = getChildMeasureSpec(getMeasuredHeight(),
+                            lp.topMargin + lp.bottomMargin,
+                            lp.height);
+                }
+
+                child.measure(childWidthMeasureSpec, childHeightMeasureSpec);
+            }
+        }
+
+
     }
+
 }
